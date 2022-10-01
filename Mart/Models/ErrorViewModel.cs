c52f3e72f0c45ad445a7d32c07821ed49ff4a0ee
betel <betel@BETEL>
2022-10-01 19:57:09 +0300

    Add project files.

---
+++ Mart/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mart.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
